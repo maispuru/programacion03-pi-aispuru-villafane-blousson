38029c682a50dc30e4b3be37abfc2c85b83e0482
maispuru <maispuru@udesa.edu.ar>
2026-06-10 18:31:04 -0300

    home

--- demo-expo/src/screens/Home.js
+++ demo-expo/src/screens/Home.js
@@ -2,10 +2,17 @@ import React from "react";
 import {useState,useEffect} from 'react'
 import { View, Text, Pressable,StyleSheet  } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { db } from '../firebase/config';
 
 const Stack = createNativeStackNavigator();
 
 function Home(){
+
+    const [Posteos, setPosteos] = useState([]);
+    
+    useEffect(() => {
+
+    })
     return(
         <View> 
             <Text >Home</Text> 
