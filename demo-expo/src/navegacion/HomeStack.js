637d3764ecc4f15fd10833e6aa482c82090463ec
matiasblousson <mblousson@udesa.edu.ar>
2026-06-19 15:16:10 -0300

    Cambios totales

--- demo-expo/src/navegacion/HomeStack.js
+++ demo-expo/src/navegacion/HomeStack.js
@@ -2,14 +2,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '../screens/Home';
 import ComentarPosteo from '../screens/ComentarPosteo';
 
-const HomeStack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 function HomeStackScreen() {
     return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="HomeFeed" component={Home} options={{ headerShown: false }}/>
-            <HomeStack.Screen name="ComentarPosteo" component={ComentarPosteo} options={{ headerShown: false }}/>
-        </HomeStack.Navigator>
+        <Stack.Navigator>
+            <Stack.Screen name="HomeFeed" component={Home} options={{ headerShown: false }}/>
+            <Stack.Screen name="ComentarPosteo" component={ComentarPosteo} options={{ headerShown: false }}/>
+        </Stack.Navigator>
     );
 }
 
